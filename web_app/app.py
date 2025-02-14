0f8b3308f536142edc12764ba6ec1b261a731f0f
VaishnaviMangalirgukt <137773385+VaishnaviMangalirgukt@users.noreply.github.com>
2025-02-14 12:56:25 +0530

    Update app.py

--- web_app/app.py
+++ web_app/app.py
@@ -89,4 +89,5 @@ def analyze():
     return jsonify({"success": True, "results": results})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
+
